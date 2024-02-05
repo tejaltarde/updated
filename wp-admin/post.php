4c387adaae37d0c86bdde8a4cf6d4aa6ca552d04
tejaltarde <tejaltarade2003@gmail.com>
2024-02-05 15:22:37 +0530

    Restored website functionality by adding index.html file

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://tejaltarde.github.io/updated//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D20013&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://tejaltarde.github.io/updated//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D20013&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://tejaltarde.github.io/updated//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D20013&reauth=1">https://tejaltarde.github.io/updated//wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FSearchinTech%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D20013&reauth=1</a></p>
+	</body>
+</html>
